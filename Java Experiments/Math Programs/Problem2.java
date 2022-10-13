e73142cd11adf0bbd964954edca2b00f63563700
Prabhjot-Bhandal <702740@pdsb.net>
2022-10-13 13:50:23 -0400

    Help

--- Java Experiments/Math Programs/Problem2.java
+++ Java Experiments/Math Programs/Problem2.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
 //Name: Prabhjot Bhandal
 //Date: October 12th, 2022
 /*Write a program that simulates the rolling of a fair die. Output what shows on the face that is up.*/
 
 public class Problem2 {
     public static void main(String[] args) {
-        
+        //Randomized diceRoll variable that has a range of 1-6 that will output a whole number
+        double diceRoll = (int)(Math.random() * 6 + 1);
+        //Prints diceRoll
+        System.out.println(diceRoll);
 }
 }
---
+++ Java Experiments/Math Programs/Problem4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+//Name: Prabhjot Bhandal
+//Date: October 12th, 2022
+/*Write a program that simulates the rolling of a fair die. Output what shows on the face that is up.*/
+
+public class Problem4 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            //Intro for a
+            System.out.print("Input a: ");
+            //Gets a
+            double a = in.nextInt();
+            //Intro for b
+            System.out.print("Input b: ");
+            //Get b
+            double b = in.nextInt();
+            //Intro for c
+            System.out.print("Input c: ");
+            //Get c
+            double c = in.nextInt();
+
+            
+        }
+    }
+}
