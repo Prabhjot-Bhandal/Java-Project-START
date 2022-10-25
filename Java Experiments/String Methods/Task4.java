40951814c883084d73ba256ba6bc5f3e082ec0d4
MetalStriker <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2022-10-25 12:37:20 -0400

    Update Task4.java

--- Java Experiments/String Methods/Task4.java
+++ Java Experiments/String Methods/Task4.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
+//Name: Prabhjot Bhandal
+//Date: October 25, 2022
+/*Purpose: For this version of the game decide on the number of players. Next, get a sentence from the 
+first player. Apply a variety of String methods to one or more String methods as the phrase is 
+“whispered” to the next player so the message is changed in some way. Apply String methods to the 
+updated message and keep whispering to the next player. Finally, show the original message and the 
+message received by the last player.*/
 
 public class Task4 {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
+            //Introduction for the numberOfPlayers
+            System.out.print("Please enter the number of players currently playing: ");
+            //Gets numberOfPlayers
+            int numberOfPlayers = in.nextInt();
+
             
         }
     }
