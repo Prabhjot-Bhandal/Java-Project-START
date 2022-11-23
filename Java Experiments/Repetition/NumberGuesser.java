55be6544961a0330a9fef449958a8634915f111d
Prabhjot-Bhandal <702740@pdsb.net>
2022-11-23 13:50:05 -0500

    he he he haw

---
+++ Java Experiments/Repetition/NumberGuesser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//Name: Prabhjot Bhandal
+//Date: November 23rd, 2022
+/*Purpose: Write a program that generates a random integer between 10 and 30 
+including 10 and 30 and then asks the user to guess the number that was 
+generated.*/
+
+public class NumberGuesser {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            
+        }
+    }
+}
