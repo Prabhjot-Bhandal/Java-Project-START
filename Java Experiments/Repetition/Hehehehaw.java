7c5c744a4c9717e8f2220c8844bd41fa01627157
MetalStriker <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2022-11-29 20:22:56 -0500

    Update Hehehehaw.java

--- Java Experiments/Repetition/Hehehehaw.java
+++ Java Experiments/Repetition/Hehehehaw.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Hehehehaw {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Please enter a positive integer.");
-            int userInput = in.nextInt();
-            int powerOfTwo = 0;
-            while (Math.pow(2,powerOfTwo) < userInput){
-                powerOfTwo++;
+            System.out.print("Please enter an integer: ");
+            int num = in.nextInt();
+            int counter = 1;
+            while (num % 2 == 0) {
+                num /= 2;
+                counter++;
             }
-            System.out.println("The smallest power of two greater than or equal to " + userInput + " is " + powerOfTwo);
-    
+            System.out.println(counter);
         }
-}
+    }
 }
