79fdc584cbf44368e1d64290e70d7350a785c2ae
Prabhjot-Bhandal <702740@pdsb.net>
2022-12-01 12:37:26 -0500

    Breh

--- Java Experiments/Repetition/QuadraticCalcRepetition.java
+++ Java Experiments/Repetition/QuadraticCalcRepetition.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Hehehehaw {
+
+public class QuadraticCalcRepetition {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             double aValue;
