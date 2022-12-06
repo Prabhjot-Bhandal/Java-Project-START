c2760d3bf7f059e52e705960cf5e8803428340c1
Prabhjot-Bhandal <702740@pdsb.net>
2022-12-06 14:20:50 -0500

    Bruh code restoration

--- Java Experiments/Repetition/TextTranslator.java
+++ Java Experiments/Repetition/TextTranslator.java
@@ -1,16 +1,72 @@
 import java.util.Scanner;
 //Name: Prabhjot Bhandal
-//Date: November 21st, 2022
+//Date: December 6th, 2022
 /*Purpose: Write a program that will continually input a short form and output the translation.*/
-
+ 
 public class TextTranslator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         try (Scanner in = new Scanner(System.in)) {
-            //Gets phrase
-            System.out.print("Enter a phrase: ");
-            String phrase = in.nextLine().toUpperCase().replaceAll(" ", "");
-
-            
+            //Intro for the program
+            System.out.println("This program translates texting abbreviations for you.\nJust type in the phrase you want to translate and the program translates it for you!\nTo stop the program, simply type \"stop\" when prompted for a phrase.\n\n");
+            Thread.sleep(3000);
+ 
+            //Initializes phrase and run
+            String phrase;
+            boolean run = true;
+ 
+            while (run == true) {
+                //Gets phrase
+                System.out.print("Enter a phrase: ");
+                //Changes whatever the user enters to upper case and takes out all spaces
+                phrase = in.nextLine().toUpperCase().replaceAll(" ", "");
+ 
+                //Abbreviations eligible for translation
+                switch (phrase) {
+                    case "CU":
+                        System.out.println("Translation: See you\n");
+                        break;
+                    case ":-)":
+                        System.out.println("Translation: I'm happy\n");
+                        break;
+                    case ":-(":
+                        System.out.println("Translation: I'm unhappy\n");
+                        break;
+                    case ";-)":
+                        System.out.println("Translation: Wink\n");
+                        break;
+                    case ":-P":
+                        System.out.println("Translation: Stick out my tongue\n");
+                        break;
+                    case "(~.~)":
+                        System.out.println("Translation: I'm sleepy\n");
+                        break;
+                    case "TA":
+                        System.out.println("Translation: Totally awesome\n");
+                        break;
+                    case "CUZ":
+                        System.out.println("Translation: Because\n");
+                        break;
+                    case "TY":
+                        System.out.println("Translation: Thank you\n");
+                        break;
+                    case "YW":
+                        System.out.println("Translation: You're welcome\n");
+                        break;
+                    case "TTYL":
+                        System.out.println("Translation: Talk to you later\n");
+                        break;
+                    //Stopping the program
+                    case "STOP":
+                        System.out.println("\n\nExit program");
+                        run = false;
+                        break;
+                    //For errors/abbreviations not in the list
+                    default:
+                    System.out.println("\n\nSorry, please enter a valid abbreviation.\n");
+                    break;
+                }
+            }//End of while loop
         }
     }
 }
+
