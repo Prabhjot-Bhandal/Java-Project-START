6c4552ff833f60ef0c01164df6ff960ba5b70f7f
Prabhjot-Bhandal <702740@pdsb.net>
2022-12-14 15:08:01 -0500

    Oml

--- Java Experiments/Methods/MethodsDemo.java
+++ Java Experiments/Methods/MethodsDemo.java
@@ -10,12 +10,7 @@ public class MethodsDemo {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //How to use the method in main
-        greeting();
-        greetingAM();
-        greetingPM();
-        getTimeOfDay();
-        System.out.println("This is the end of the road for us Waltuh.");
+        gettingStarted();
 
     } //main
 
@@ -29,24 +24,47 @@ public class MethodsDemo {
 
     }// nameOfMethod/MethodIdentifier*/
 
-    public static void greeting() {
-        System.out.println("I'm afraid and hopless since I didn't have a clue of what to do.");
+    public static void gettingStarted() {
+        String time = getTimeOfDay();
 
-    } //greeting
-
-    public static void greetingAM() {
-        System.out.println("It's beaUTIFUL DAY HERE IN LOS ANGELES.\nTHERE IS CURRENTLY A DROUGHT, BE CAREFUL AND HAVE A GOOD DAY!");
-
-    } //Morning greeting
-
-    public static void greetingPM() {
-        System.out.println("NO SUN DON'T LEAVE ME");
-
-    } //Evening greeting
-
-    public static String getTimeOfDay() {
-        System.out.print("Please enter the time of day: ");
-        String timeOfDay = in.next();
-        return timeOfDay;
+        //If morning or noon
+        if (time.equalsIgnoreCase("morning") || time.equalsIgnoreCase("noon")) {
+            greetingAM();
+        }
+        //If afternoon, evening or night
+        else if (time.equalsIgnoreCase("afternoon") || time.equalsIgnoreCase("evening") || time.equalsIgnoreCase("night")) {
+            greetingPM();
+        }
+        //If anything else
+        else {
+            greeting();
+        }
     }
+        public static void greeting() {
+            System.out.println("\nHello.");
+
+        } //greeting
+
+        public static void greetingAM() {
+            System.out.println("\nIt's beaUTIFUL DAY HERE IN LOS ANGELES.\nTHERE IS CURRENTLY A DROUGHT, BE CAREFUL AND HAVE A GOOD DAY!");
+
+        } //Morning greeting
+
+        public static void greetingPM() {
+            System.out.println("\nNO SUN DON'T LEAVE ME");
+
+        } //Evening greeting
+
+        public static String getTimeOfDay() {
+            //Gets timeOfDay
+            System.out.print("Please enter the time of day: ");
+            //Changes the whole string to lower case and takes out all the spaces
+            String timeOfDay = in.next().toLowerCase().replaceAll(" ", "");
+            return timeOfDay;
+        }
+
+    public static void hourOfTheDay() {
+        
+    }
+    
 }
