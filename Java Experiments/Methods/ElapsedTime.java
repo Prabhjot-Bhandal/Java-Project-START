793300862500aa297a4a0246ac96235bd7e9630d
Prabhjot-Bhandal <702740@pdsb.net>
2022-12-19 13:44:48 -0500

    Your mother

--- Java Experiments/Methods/ElapsedTime.java
+++ Java Experiments/Methods/ElapsedTime.java
@@ -4,42 +4,22 @@ import java.util.Scanner;
 //Write a method called elapsedTime() that returns the difference of two integer parameters
 
 public class ElapsedTime {
-    //Makes in a class variable
+    //Makes in and actvity class variables
     static Scanner in = new Scanner(System.in);
+    static String activity = "";
     public static void main(String[] args) {
         //Initializes startTime, endTime and activity
         //Initialized at -1 since 0 is a valid time
         int startTime = -1;
         int endTime = -1;
-        String activity = "";
 
         //Get activity
         System.out.print("\nPlease enter the name of the activity you were doing: ");
-        activity = in.next().replaceAll(" ", "");
+        activity = in.nextLine().replaceAll(" ", "");
 
-        //Loops back if either startTime are not within the 24-hour clock
-        do {
-            //Get startTime
-            System.out.print("\nPlease enter the start time of your " + activity + " activity (in the 24-hour clock): ");
-            startTime = in.nextInt();
-            //Tells user input is invalid if startTime is less than 0 or greater than 23
-            if (startTime < 0 || startTime > 23) {
-                System.out.print("Invalid input. Please input a vaild hour (0-23).\n\n");
-            }
-
-        } while (startTime < 0 || startTime > 23);
-
-        //Loops back if either endTime are not within the 24-hour clock
-        do {
-            //Get endTime 
-            System.out.print("\nPlease enter the end time of your " + activity + " activity (in the 24-hour clock): ");
-            endTime = in.nextInt();
-             //Tells user input is invalid if endTime is less than 0 or greater than 23
-             if (endTime < 0 || endTime > 23) {
-                System.out.print("Invalid input. Please input a vaild hour (0-23).\n\n");
-            }
-
-        } while (endTime < 0 || endTime > 23);
+        //Calls getHour() method to get both start and end time
+        startTime = getHour("start time");
+        endTime = getHour("end time");
 
         //Calls getElapsedTime() and sets the return value to elapsedTime
         int elapsedTime = getElapsedTime(startTime, endTime);
@@ -49,7 +29,31 @@ public class ElapsedTime {
     } //main
     
 
+    public static int getHour(String timeString) {
+        /*Takes in a string to tell the user whether they are inputting the start or end time of their 
+        activity.
+        Returns the start or end time the user inputs and verifies if the user has put in a legitimate hour*/
+
+        //Initializes hour
+        int hour;
+        //Loops back if either startTime are not within the 24-hour clock
+        do {
+            //Gets hour
+            System.out.print("\nPlease enter the " + timeString + " of your " + activity + " activity (in the 24-hour clock): ");
+            hour = in.nextInt();
+             //Tells user input is invalid if endTime is less than 0 or greater than 23
+             if (hour < 0 || hour > 23) {
+                System.out.print("Invalid input. Please input a vaild hour (0-23).\n\n");
+            }
+
+        } while (hour < 0 || hour > 23);
+        return hour;
+    } //getHour
+
     public static int getElapsedTime(int start, int end) {
+        /*Takes in the starting hour of the activity and the ending hour of the activity,
+         returns how long someone has spent doing their activity*/
+
         //Initializes elpasedTime at -1 since 0 is a valid integer
         int elapsedTime = -1;
 
