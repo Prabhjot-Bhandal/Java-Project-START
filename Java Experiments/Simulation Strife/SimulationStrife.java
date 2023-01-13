8cbced558ea48759d229bc6de517dbc787ff5055
Prabhjot-Bhandal <702740@pdsb.net>
2023-01-13 13:50:26 -0500

    Final Project START!

---
+++ Java Experiments/Simulation Strife/SimulationStrife.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+//Prabhjot Bhandal
+//January 13th, 2022
+/*Simulates a text and turn-based battle against an enemy, but everytime the program runs, 
+ it's a different scenario*/
+
+public class SimulationStrife {
+    //Static variables that can be used across the enitre program
+    static Scanner in = new Scanner(System.in);
+ public static void main(String[] args) throws InterruptedException {
+    //Game loop boolean
+    boolean playing = introduction();
+ } //main
+
+ public static boolean introduction() throws InterruptedException{
+    //Introduces the game, asks the user if they want to play, returns a boolean
+
+    
+ } //Introductions
+}
