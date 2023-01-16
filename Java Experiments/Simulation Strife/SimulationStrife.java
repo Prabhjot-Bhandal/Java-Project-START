919ba3398ab984d8607464dd2c6418acb737a977
MetalStriker <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2023-01-16 14:06:56 -0500

    Bruh I forgot

--- Java Experiments/Simulation Strife/SimulationStrife.java
+++ Java Experiments/Simulation Strife/SimulationStrife.java
@@ -9,12 +9,22 @@ public class SimulationStrife {
     static Scanner in = new Scanner(System.in);
  public static void main(String[] args) throws InterruptedException {
     //Game loop boolean
-    boolean playing = introduction();
+    boolean run = introduction();
  } //main
 
  public static boolean introduction() throws InterruptedException{
     //Introduces the game, asks the user if they want to play, returns a boolean
+    System.out.println("");
+    //Initializes runQuestion
+    String runQuestion = "";
+    Thread.sleep(3000);
+    //Loop keeps running if the user does not input Y or N
+    do {
+      //Updates runQuestion with user input
+      System.out.print("\nDo you want to play? (Y/N): ");
+      runQuestion = in.next();
 
+    } while (runQuestion.equalsIgnoreCase("Y") || runQuestion.equalsIgnoreCase("N")); //End of do-while
     
  } //Introductions
 }
