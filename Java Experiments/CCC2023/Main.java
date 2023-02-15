2474de5f4dbbee66d9d816b1225b6fd262e11d41
Prabhjot-Bhandal <702740@pdsb.net>
2023-02-15 11:42:35 -0500

    Death

---
+++ Java Experiments/CCC2023/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+//Prabhjot Bhandal
+//February 15th, 2023
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
