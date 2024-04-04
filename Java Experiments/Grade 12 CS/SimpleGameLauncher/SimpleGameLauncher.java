6196a462a15e66e9a80b9c9c5be5bfed862c150b
Prabhjot-Bhandal <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2024-04-04 15:07:47 -0400

    Les go

---
+++ Java Experiments/Grade 12 CS/SimpleGameLauncher/SimpleGameLauncher.java
@@ -0,0 +1,19 @@
+package SimpleGameLauncher;
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleGameLauncher {
+    //Crates the mainFrame that houses everything, static so all methods can access it
+    static JFrame mainFrame = new JFrame();
+    public static void main(String[] args) {
+        
+    } //main
+
+    public static void gameScrollLibrary() {
+        /*Action: Makes the right most panel that houses all of the user's games
+          Input: None
+          Output: Creates the library scrolling panel*/
+
+        
+    } //gameScrollLibrary
+}
