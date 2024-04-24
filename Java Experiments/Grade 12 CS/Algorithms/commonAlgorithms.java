3658b79deffc054bec66e8e407229b143da6f4cd
MetalStriker <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2024-04-24 14:31:22 -0400

    a

--- Java Experiments/Grade 12 CS/Algorithms/commonAlgorithms.java
+++ Java Experiments/Grade 12 CS/Algorithms/commonAlgorithms.java
@@ -6,11 +6,10 @@ public class commonAlgorithms {
 
         //int indexOfValue = linearSearch(72, ages);
         //System.out.println("\nThe index will be: " + indexOfValue);
-        insertionSort(ages);
-        System.out.println(verifySorting(ages));
+        //insertionSort(ages);
+        System.out.println(verifySortingInt(ages));
 
-
-    }
+    } //main
 
     public static int linearSearch(int searchValue, int[] array) {
         /*Action: Searches through an array for a given value.
@@ -40,8 +39,8 @@ public class commonAlgorithms {
         }
     }
 
-    public static int[] insertionSort(int[] intArray) {
-        /*Action: Sorts a given array using the insertionSort algorithm
+    public static int[] insertionSortInt(int[] intArray) {
+        /*Action: Sorts a given integer array using the insertionSort algorithm
           Input: One dimensional int array
           Output: The sorted one dimensional array*/
 
@@ -68,7 +67,69 @@ public class commonAlgorithms {
         //Returns the array
         return intArray;
         
-    } //insertionArray
+    } //insertionSortInt
+
+    public static char[] insertionSortChar(char[] charArray) {
+        /*Action: Sorts a given char array using the insertionSort algorithm
+          Input: One dimensional int array
+          Output: The sorted one dimensional array*/
+
+        //Loops through the entire given char array
+        for (int index = 1 ; index < charArray.length ; index++) {
+            //Stores the current element the loop is on in an char variable
+            char item = charArray[index];
+            //Stores the current index in a seperate variable so that it can be used to swap later on
+            int toPlace = index;
+
+            /*While loop runs through all previous indices before the current one and compares the 
+            current element to all the previous ones*/
+            while (toPlace > 0 && item < charArray[toPlace - 1]) {
+                //Assigns previous (and thus lower) elements before the current element in the array
+                charArray[toPlace] = charArray[toPlace - 1];
+                //Subtracts 1 so that we can look at the next element, works from the current index to the first
+                toPlace--;
+            } //While loop stops once the current comparison item is no longer smaller than the one before it
+
+            //Switches the current element to the index where all of the ones after it are greater than it
+            charArray[toPlace] = item;
+         }//for index
+
+        //Returns the array
+        return charArray;
+        
+    } //insertionSortChar
+
+    public static String[] insertionSortStr(String[] strArray) {
+        /*Action: Sorts a given String array using the insertionSort algorithm
+          Input: One dimensional String array
+          Output: The sorted one dimensional array*/
+
+        //Loops through the entire given char array
+        for (int index = 1 ; index < strArray.length ; index++) {
+            //Stores the current element the loop is on in an char variable
+            String item = strArray[index];
+            //Stores the current index in a seperate variable so that it can be used to swap later on
+            int toPlace = index;
+
+            /*While loop runs through all previous indices before the current one and compares the 
+            current element to all the previous ones*/
+            //Less than for strings .compareToIgnoreCase is negative
+            while (toPlace > 0 && item.compareToIgnoreCase(strArray[toPlace - 1]) < 0) {
+                //Assigns previous (and thus lower) elements before the current element in the array
+                strArray[toPlace] = strArray[toPlace - 1];
+                //Subtracts 1 so that we can look at the next element, works from the current index to the first
+                toPlace--;
+            } //While loop stops once the current comparison item is no longer smaller than the one before it
+
+            //Switches the current element to the index where all of the ones after it are greater than it
+            strArray[toPlace] = item;
+         }//for index
+
+        //Returns the array
+        return strArray;
+        
+    } //insertionSortStr
+
 
     public static int[] bubbleSort(int[] intArray) {
         /*Action: Sorts an int one-dimensional array using the bubble sort
@@ -97,7 +158,7 @@ public class commonAlgorithms {
     } //bubbleSort
 
 
-    public static boolean verifySorting(int[] intArray) {
+    public static boolean verifySortingInt(int[] intArray) {
         /*Action: Checks if an array is sorted by looking at an element and checking if the
           element before is greater than the current element.
           Input: intArray
@@ -113,7 +174,44 @@ public class commonAlgorithms {
 
         //If the loop runs fine, that means the entire array IS sorted
         return true;
-    } //verifySorting
+    } //verifySortingInt
+
+    public static boolean verifySortingChar(char[] charArray) {
+        /*Action: Checks if an array is sorted by looking at an element and checking if the
+          element before is greater than the current element.
+          Input: charArray
+          Output: A boolean denoting if the array is sorted or not.*/
+
+        //Starts at index 1 so that it can compare to the element before the current one
+        for (int index = 1; index < charArray.length; index++) {
+            //If the element before the current one is greater, the array is NOT sorted
+            if (charArray[index] < charArray[index - 1]) {
+                return false;
+            }
+        }
+
+        //If the loop runs fine, that means the entire array IS sorted
+        return true;
+    } //verifySortingChar
+
+
+    public static boolean verifySortingStr(String[] strArray) {
+        /*Action: Checks if an array is sorted by looking at an element and checking if the
+          element before is greater than the current element.
+          Input: strArray
+          Output: A boolean denoting if the array is sorted or not.*/
+
+        //Starts at index 1 so that it can compare to the element before the current one
+        for (int index = 1; index < strArray.length; index++) {
+            //If the element before the current one is greater, the array is NOT sorted
+            if (strArray[index].compareToIgnoreCase(strArray[index - 1]) < 0) {
+                return false;
+            }
+        }
+
+        //If the loop runs fine, that means the entire array IS sorted
+        return true;
+    } //verifySortingStr
 
     
 }
