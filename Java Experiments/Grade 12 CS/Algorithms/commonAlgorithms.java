c4e204f062fc820cecc832dac2f494ddca583fc9
MetalStriker <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2024-04-30 13:46:37 -0400

    yeah

--- Java Experiments/Grade 12 CS/Algorithms/commonAlgorithms.java
+++ Java Experiments/Grade 12 CS/Algorithms/commonAlgorithms.java
@@ -6,7 +6,8 @@ public class commonAlgorithms {
 
         //int indexOfValue = linearSearch(72, ages);
         //System.out.println("\nThe index will be: " + indexOfValue);
-        insertionSortInt(ages);
+        //selectionSort(ages);
+        printOneDIntArray(ages);
         System.out.println(verifySortingInt(ages));
         System.out.println(binarySearch(4, ages));
         //System.out.println(binarySearchRecursive(ages, 4, 0, ages.length - 1));
@@ -172,7 +173,7 @@ public class commonAlgorithms {
 
     } //binarySearchRecursive
 
-    public static int[] selectionSort(int[] intArray) {
+    public static int[] selectionSortInt(int[] intArray) {
         /*Action: Sorts a given integer array using the selection sort algorithm.
           Input: An unsorted integer array.
           Output: An integer array sorted in ascending order.*/
@@ -186,8 +187,9 @@ public class commonAlgorithms {
             int smallestIndex = comparisonIndex;
 
             /*Loops through all the indices after the current comparison index and compares them
-              to the current comparison index, finds the smallest value in that current instance.*/
-            for (int comparingToIndex = 1; comparingToIndex < intArray.length; comparingToIndex++) {
+              to the current comparison index, finds the smallest value in that current instance.
+              The comparingToIndex is 1 above the comparisonIndex*/
+            for (int comparingToIndex = comparisonIndex + 1; comparingToIndex < intArray.length; comparingToIndex++) {
                 //If the current comparingToIndex has an element smaller than the current smallest element, overwrites them
                 if (intArray[comparingToIndex] < smallestElement) {
                     //Sets the smallestElement to the current comparingTo index
@@ -211,7 +213,94 @@ public class commonAlgorithms {
         //Returns the array after going through all the indices
         return intArray;
         
-    } //selectionSort
+    } //selectionSortInt
+
+    public static char[] selectionSortChar(char[] charArray) {
+        /*Action: Sorts a given char array using the selection sort algorithm.
+          Input: An unsorted char array.
+          Output: An char array sorted in ascending order.*/
+
+        //Loops through the entire array so that we get the smallest index compared to each index
+        for (int comparisonIndex = 0; comparisonIndex < charArray.length - 1; comparisonIndex++) {
+            //Holds the index that has the smallest value in the array COMPARED TO the current comparison index
+            //The initial smallestElement is the current comparison index's value
+            char smallestElement = charArray[comparisonIndex];
+            //Initial smallestIndex is the current comparison index
+            int smallestIndex = comparisonIndex;
+
+            /*Loops through all the indices after the current comparison index and compares them
+              to the current comparison index, finds the smallest value in that current instance.
+              The comparingToIndex is 1 above the comparisonIndex*/
+            for (int comparingToIndex = comparisonIndex + 1; comparingToIndex < charArray.length; comparingToIndex++) {
+                //If the current comparingToIndex has an element smaller than the current smallest element, overwrites them
+                if (charArray[comparingToIndex] < smallestElement) {
+                    //Sets the smallestElement to the current comparingTo index
+                    smallestElement = charArray[comparingToIndex];
+                    //Sets the smallestIndex to the index in the array that has the new smallest element
+                    smallestIndex = comparingToIndex;
+
+                } //Overwriting smallestElement and Index if statement
+
+            } //comparingTo for loop
+
+            //Creates a temporary char variable to hold the value of the comparisonIndex in the array
+            char temp = charArray[comparisonIndex];
+            //Sets the comparisonIndex's element to the new smallest element
+            charArray[comparisonIndex] = smallestElement;
+            //Sets the index that had the smallest element equal to the element that was at the comparisonIndex prior to overwriting
+            charArray[smallestIndex] = temp;
+
+        } //comparisonIndex for loop
+
+        //Returns the array after going through all the indices
+        return charArray;
+        
+    } //selectionSortChar
+
+    public static String[] selectionSortStr(String[] strArray) {
+        /*Action: Sorts a given string array using the selection sort algorithm.
+          Input: An unsorted string array.
+          Output: An string array sorted in ascending order.*/
+
+        //Loops through the entire array so that we get the smallest index compared to each index
+        for (int comparisonIndex = 0; comparisonIndex < strArray.length - 1; comparisonIndex++) {
+            //Holds the index that has the smallest value in the array COMPARED TO the current comparison index
+            //The initial smallestElement is the current comparison index's value
+            String smallestElement = strArray[comparisonIndex];
+            //Initial smallestIndex is the current comparison index
+            int smallestIndex = comparisonIndex;
+
+            /*Loops through all the indices after the current comparison index and compares them
+              to the current comparison index, finds the smallest value in that current instance.
+              The comparingToIndex is 1 above the comparisonIndex*/
+            for (int comparingToIndex = comparisonIndex + 1; comparingToIndex < strArray.length; comparingToIndex++) {
+                //If the current comparingToIndex has an element smaller than the current smallest element, overwrites them
+                //Less than for strings .compareTo is negative
+                if (strArray[comparingToIndex].compareTo(smallestElement) < 0) {
+                    //Sets the smallestElement to the current comparingTo index
+                    smallestElement = strArray[comparingToIndex];
+                    //Sets the smallestIndex to the index in the array that has the new smallest element
+                    smallestIndex = comparingToIndex;
+
+                } //Overwriting smallestElement and Index if statement
+
+            } //comparingTo for loop
+
+            //Creates a temporary string variable to hold the value of the comparisonIndex in the array
+            String temp = strArray[comparisonIndex];
+            //Sets the comparisonIndex's element to the new smallest element
+            strArray[comparisonIndex] = smallestElement;
+            //Sets the index that had the smallest element equal to the element that was at the comparisonIndex prior to overwriting
+            strArray[smallestIndex] = temp;
+
+        } //comparisonIndex for loop
+
+        //Returns the array after going through all the indices
+        return strArray;
+        
+    } //selectionSortStr
+
+
 
     public static int[] insertionSortInt(int[] intArray) {
         /*Action: Sorts a given integer array using the insertionSort algorithm
@@ -287,8 +376,8 @@ public class commonAlgorithms {
 
             /*While loop runs through all previous indices before the current one and compares the 
             current element to all the previous ones*/
-            //Less than for strings .compareToIgnoreCase is negative
-            while (toPlace > 0 && item.compareToIgnoreCase(strArray[toPlace - 1]) < 0) {
+            //Less than for strings .compareTo is negative
+            while (toPlace > 0 && item.compareTo(strArray[toPlace - 1]) < 0) {
                 //Assigns previous (and thus lower) elements before the current element in the array
                 strArray[toPlace] = strArray[toPlace - 1];
                 //Subtracts 1 so that we can look at the next element, works from the current index to the first
@@ -304,13 +393,12 @@ public class commonAlgorithms {
         
     } //insertionSortStr
 
+    public static int[] bubbleSortInt(int[] intArray) {
+        /*Action: Sorts an int one-dimensional array using the bubble sort algorithm
+          Input: An unsorted intArray
+          Output: The sorted intArray in ascending order*/
 
-    public static int[] bubbleSort(int[] intArray) {
-        /*Action: Sorts an int one-dimensional array using the bubble sort
-          Input: intArray
-          Output: The sorted intArray*/
-
-        //Creates a temporary variable to hold an index for comparisons
+        //Creates a temporary variable to hold the element at the comparison index
         int temp;
         //Looks at the first index
         for (int index1 = 0; index1 < intArray.length; index1++){
@@ -321,7 +409,8 @@ public class commonAlgorithms {
                 //Compares index 2's element to the one before it
                 if(intArray[index2] < intArray[index2-1]){
                     //If the one before index 2 is larger then it, they swap
-                    intArray[index2-1] = intArray[index2];
+                    intArray[index2 - 1] = intArray[index2];
+                    //Swaps the element before index2 in the array to index2
                     intArray[index2]  = temp;			
                 }//if			
             }//for index2 	
@@ -329,8 +418,64 @@ public class commonAlgorithms {
 
         //Returns the array
         return intArray;
-    } //bubbleSort
 
+    } //bubbleSortInt
+
+    public static char[] bubbleSortChar(char[] charArray) {
+        /*Action: Sorts a char one-dimensional array using the bubble sort algorithm
+          Input: An unsorted charArray
+          Output: The sorted charArray in ascending order*/
+
+        //Creates a temporary variable to hold the element at the comparison index
+        char temp;
+        //Looks at the first index
+        for (int index1 = 0; index1 < charArray.length; index1++){
+            //Looks at the index right after the first index
+            for(int index2 = 1; index2 < charArray.length; index2++){
+                //Sets the temp variable equal to the value before index 2 in the array
+                temp = charArray[index2 - 1];
+                //Compares index 2's element to the one before it
+                if(charArray[index2] < charArray[index2 - 1]){
+                    //If the one before index 2 is larger then it, they swap
+                    charArray[index2 - 1] = charArray[index2];
+                    //Swaps the element before index2 in the array to index2
+                    charArray[index2]  = temp;			
+                }//if			
+            }//for index2 	
+        }//for index1
+
+        //Returns the array
+        return charArray;
+
+    } //bubbleSortChar
+
+    public static String[] bubbleSortStr(String[] strArray) {
+        /*Action: Sorts a string one-dimensional array using the bubble sort algorithm
+          Input: An unsorted strArray
+          Output: The sorted strArray in ascending order*/
+
+        //Creates a temporary variable to hold the element at the comparison index
+        String temp;
+        //Looks at the first index
+        for (int index1 = 0; index1 < strArray.length; index1++){
+            //Looks at the index right after the first index
+            for(int index2 = 1; index2 < strArray.length; index2++){
+                //Sets the temp variable equal to the value before index 2 in the array
+                temp = strArray[index2 - 1];
+                //Compares index 2's element to the one before it
+                if(strArray[index2].compareTo(strArray[index2 - 1]) < 0){
+                    //If the one before index 2 is larger then it, they swap
+                    strArray[index2 - 1] = strArray[index2];
+                    //Swaps the element before index2 in the array to index2
+                    strArray[index2]  = temp;			
+                }//if			
+            }//for index2 	
+        }//for index1
+
+        //Returns the array
+        return strArray;
+
+    } //bubbleSortStr
 
 }
 
