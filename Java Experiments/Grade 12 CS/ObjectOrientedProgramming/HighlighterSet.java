f2e2d605bd1b34f41b1b4a62458ec02822dc8eff
Prabhjot-Bhandal <92166898+Prabhjot-Bhandal@users.noreply.github.com>
2024-06-03 13:50:17 -0400

    aq

--- Java Experiments/Grade 12 CS/ObjectOrientedProgramming/HighlighterSet.java
+++ Java Experiments/Grade 12 CS/ObjectOrientedProgramming/HighlighterSet.java
@@ -4,90 +4,127 @@
 package ObjectOrientedProgramming;
 
 public class HighlighterSet {
-    //INSTANCE VARIABLES
-    private Highlighter[] highlighterArray; //Actually holds the array
-    private int arrayLength; //Length of the highlighter array
-    final String[] COLOURS = {"red", "blue", "yellow", "green", "purple", "orange", "grey", "black", "white"}; //Array containing the possible colours of highlighter
+  //INSTANCE VARIABLES
+  private Highlighter[] highlighterArray; //Actually holds the array
+  private int arrayLength; //Length of the highlighter array
+  final String[] COLOURS = {"red", "blue", "yellow", "green", "purple", "orange", "grey", "black", "white"}; //Array containing the possible colours of highlighter
 
 
-    //CONSTRUCTORS
-    public HighlighterSet() {
-        /*Action: Constructs an array of highlighter objects using the default highlighter constructor.
-          This will be a set of blank highlighters.
-          Input: None
-          Output: A filled highlighterArray of blank highlighters.*/
+  //CONSTRUCTORS
+  public HighlighterSet() {
+      /*Action: Constructs an array of highlighter objects using the default highlighter constructor.
+        This will be a set of blank highlighters.
+        Input: None
+        Output: A filled highlighterArray of blank highlighters.*/
 
-        /*Determines a random array length from 0 to 15 elements*/
-        arrayLength = (int) (Math.random() * 16);
-        //Instatiates the array's elements with the proper length of the array
-        highlighterArray = new Highlighter[arrayLength];
+      /*Determines a random array length from 0 to 15 elements*/
+      arrayLength = (int) (Math.random() * 16);
+      //Instatiates the array's elements with the proper length of the array
+      highlighterArray = new Highlighter[arrayLength];
 
-        //Goes through each index of the highlighterArray and creates a blank highlighter for each index
-        for (int index = 0; index < highlighterArray.length; index++) {
-            highlighterArray[index] = new Highlighter();
-        }
+      //Goes through each index of the highlighterArray and creates a blank highlighter for each index
+      for (int index = 0; index < highlighterArray.length; index++) {
+          highlighterArray[index] = new Highlighter();
+      }
 
-    } //HighlighterSet Constructor: Blank
+  } //HighlighterSet Constructor: Blank
 
-    public HighlighterSet(int arrayLength) {
-        /*Action: Constructs a set of highlighters with random colours and average dimensions IF the array length is not 9.
-          Input: arrayLength
-          Output: A filled highlighterArray with average size highlighters with random colouring,
-                  otherwise, it is an array of 9 highlighter objects, each with average dimensions and different
-                  colours.*/
-        
-        //Sets the the current array's arrayLength to the one given
-        this.arrayLength = arrayLength; 
-        //Instatiates the array's elements with the proper length of the array
-        highlighterArray = new Highlighter[arrayLength];
-
-        /*If the array length is equal to the length of the colours array, it will create a set of average-size
-          highlighters each with a different colour.*/
-        if (arrayLength == COLOURS.length) {
-            /*Goes through each index of the array and creates a highlighter object with average dimensions
-              a colour.*/
-            for (int index = 0; index < highlighterArray.length; index++) {
-                highlighterArray[index] = new Highlighter(COLOURS[index]);
-            }
-        }  
-
-        /*Goes through each index of the highlighter array, generates a random colour and creates
-          an average size highlighter with that colour*/
-        for (int index = 0; index < highlighterArray.length; index++) {
-            highlighterArray[index] = new Highlighter(COLOURS[(int)(Math.random() * COLOURS.length)]);
-        }
-
-    } //HighlighterSet Constructor: Average Dimensions with Random Colours or A Complete Highlighter Set
-
-
-    //GETTERS
-    public Highlighter[] getHighlighterArray() {
-        /*Action: Gets an entire highlighter array.
-          Input: A highlighterSet
-          Output: The array.*/
-        return highlighterArray;
-
-    } //getHighlighterArray
-
-    public int getArrayLength() {
-        /*Action: Gets the length of a specific highlighter array as it cannot be changed outside
-          of the highlighterSet class.
-          Input: A highlighterSet
-          Output: The length of the array.*/
-        return arrayLength;
-
-    } //getArrayLength
-
-
-    //TOSTRING METHOD
-    public String toString() {
-      /*Action: */
-        
-
+  public HighlighterSet(int arrayLength) {
+      /*Action: Constructs a set of highlighters with random colours and average dimensions IF the array length is not 9.
+        Input: arrayLength
+        Output: A filled highlighterArray with average size highlighters with random colouring,
+                otherwise, it is an array of 9 highlighter objects, each with average dimensions and different
+                colours.*/
       
+      //Sets the the current array's arrayLength to the one given
+      this.arrayLength = arrayLength; 
+      //Instatiates the array's elements with the proper length of the array
+      highlighterArray = new Highlighter[arrayLength];
+
+      /*If the array length is equal to the length of the colours array, it will create a set of average-size
+        highlighters each with a different colour.*/
+      if (arrayLength == COLOURS.length) {
+          /*Goes through each index of the array and creates a highlighter object with average dimensions
+            a colour.*/
+          for (int index = 0; index < highlighterArray.length; index++) {
+              highlighterArray[index] = new Highlighter(COLOURS[index]);
+          }
+      }  
+
+      /*Goes through each index of the highlighter array, generates a random colour and creates
+        an average size highlighter with that colour*/
+      for (int index = 0; index < highlighterArray.length; index++) {
+          highlighterArray[index] = new Highlighter(COLOURS[(int)(Math.random() * COLOURS.length)]);
+      }
+
+  } //HighlighterSet Constructor: Average Dimensions with Random Colours or A Complete Highlighter Set
+
+
+  //GETTERS
+  public Highlighter[] getHighlighterArray() {
+      /*Action: Gets an entire highlighter array.
+        Input: A highlighterSet
+        Output: The array.*/
+      return highlighterArray;
+
+  } //getHighlighterArray
+
+  public int getArrayLength() {
+      /*Action: Gets the length of a specific highlighter array as it cannot be changed outside
+        of the highlighterSet class.
+        Input: A highlighterSet
+        Output: The length of the array.*/
+      return arrayLength;
+
+  } //getArrayLength
+
+  
+  //SETTERS
+  public int setArrayLength(int arrayLength) {
+    /*Action: Sets the array length to whatever the user inputs IF it is between 0 and 16 elements.
+      Otherwise, it sets an automatic value of 3.
+      Input: An integer
+      Output: An altered arrayLength.*/
+
+    //If inputted arrayLength is within the valid range, sets the arrayLength to the new one
+    if (arrayLength > 0 && arrayLength < 16) {
+      this.arrayLength = arrayLength;
+    } 
+    //If an invalid arrayLength is inputted, sets the arrayLength to an automatic number of 3
+    else {
+      arrayLength = 3;
     }
 
+  } //setArrayLength
 
-    //INSTANCE METHODS
-    public Highlighter[] selection
+
+  //TOSTRING METHOD
+  public String toString() {
+    /*Action: Creates a string representation of the array of highlighters.
+      Input: The array of highlighters (highlighterSet)
+      Output: The highlighterSet's array length and the features of each highlighter object
+      the array holds.*/
+
+    //Creates a string that will hold the string that will be returned
+    String returnString = ("This set has " + arrayLength + " highlighters. \n");
+    //Instantiates a string variable that will hold the current highlighter object's string representation
+    String currentHighlighter;
+
+    //Goes through each element in the highlighterArray
+    for (int index = 0; index < highlighterArray.length; index++) {
+      /*Numbers the current highlighter and appends the the current highlighter's string representation
+        to its number.*/
+      currentHighlighter = ("Highlighter " + index + ":\n" + highlighterArray[index].toString());
+      //Concatenates the current highlighter's string representation to the string that will be returned
+      returnString.concat(currentHighlighter + "\n\n");
+    }
+
+    //Returns the now filled returnString will all string representations of every highlighter in the array
+    return returnString;
+
+  } //toString
+
+
+  //INSTANCE METHODS
+  public Highlighter[] selection
 }
